a0a8171a1103b5bec35c6e384045c7e02bbd4498
Gyuseon Sim <118058218+gyuseon25@users.noreply.github.com>
2025-02-20 13:34:57 +0900

    [Gold IV] Title: 오큰수, Time: 944 ms, Memory: 142464 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -8,26 +8,32 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
 
+        int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n];
-        int[] NGE = new int[n]; 
+        int[] NGE = new int[n];
+        Arrays.fill(NGE, -1);
         Stack<Integer> stack = new Stack<>();
+
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        for(int i = n - 1; i >= 0; i--) {
-            while (!stack.isEmpty() && stack.peek() <= arr[i]) {
+        for(int i = n-1; i >= 0; i--) {
+            while(!stack.isEmpty() && stack.peek() <= arr[i]) {
                 stack.pop();
             }
-
-            NGE[i] = stack.isEmpty() ? -1 : stack.peek();
-            
+            if(stack.isEmpty()) {
+                NGE[i] = -1;
+                stack.push(arr[i]);
+                continue;
+            }
+            NGE[i] = stack.peek();
             stack.push(arr[i]);
         }
+
 
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
