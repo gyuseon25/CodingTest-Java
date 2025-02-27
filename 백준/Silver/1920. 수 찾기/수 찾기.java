7233f97fa6771a891f0f5b8300c3aa755bff6a9d
Gyuseon Sim <118058218+gyuseon25@users.noreply.github.com>
2025-02-27 13:17:19 +0900

    [Silver IV] Title: 수 찾기, Time: 1200 ms, Memory: 53236 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            boolean find = false;
+            int target = Integer.parseInt(st.nextToken());
+
+            int start = 0;
+            int end = n - 1;
+            while (start <= end) {
+                int midIndex = (start + end) / 2;
+                int midValue = arr[midIndex];
+
+                if (midValue > target) {
+                    end = midIndex - 1;
+                } else if (midValue < target) {
+                    start = midIndex + 1;
+                } else {
+                    find = true;
+                    break;
+                }
+            }
+            if (find) {
+                System.out.println(1);
+            } else {
+                System.out.println(0);
+            }
+        }
+    }
+}
