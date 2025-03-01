1b9542be2e2f08aaeeff1d541f2994653dbca136
Gyuseon Sim <118058218+gyuseon25@users.noreply.github.com>
2025-03-01 19:37:45 +0900

    [Silver III] Title: 소수 구하기, Time: 756 ms, Memory: 37436 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int[] arr = new int[n+1];
+        for(int i = 2; i <= n; i++) {
+            arr[i] = i;
+        }
+
+        for(int i = 2; i <= Math.sqrt(n); i++) {
+            if(arr[i] == 0) {
+                continue;
+            }
+            for(int j = i + i; j <= n; j = j+i) {
+                arr[j] = 0;
+            }
+        }
+
+        for(int i = m; i <= n; i++) {
+            if(arr[i] != 0)
+                System.out.println(arr[i]);
+        }
+    }
+}
