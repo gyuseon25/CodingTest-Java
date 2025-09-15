2627da4bf328c9c1b58c52ac59e674f05595d77d
Gyuseon Sim <118058218+gyuseon25@users.noreply.github.com>
2025-09-15 17:05:58 +0900

    [level 2] Title: 행렬의 곱셈, Time: 10.85 ms, Memory: 90.7 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -1,21 +1,17 @@
+import java.util.*;
+
 class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
-        int r1 = arr1.length;
-        int c1 = arr1[0].length;
-        int r2 = arr2.length;
-        int c2 = arr2[0].length;
+        int[][] answer = new int[arr1.length][arr2[0].length];
         
-        
-        int[][] answer = new int[r1][c2];
-        
-        for(int i = 0; i < r1; i++) {
-            for(int j = 0; j < c2; j++) {
-                for(int k = 0; k < c1; k++) {
-                    answer[i][j] += arr1[i][k] * arr2[k][j]; 
+        for(int i = 0; i < arr1.length; i++) {
+            for(int j = 0; j < arr2[0].length; j++) {
+                for(int k = 0; k < arr2.length; k++){
+                    answer[i][j] += arr1[i][k] * arr2[k][j];
                 }
             }
         }
-            
+        
         return answer;
     }
 }
