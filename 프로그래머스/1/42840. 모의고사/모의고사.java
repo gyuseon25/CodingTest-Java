686ba48dcc55b10641c951589edfcb963e2d1c63
Gyuseon Sim <118058218+gyuseon25@users.noreply.github.com>
2025-09-15 16:36:03 +0900

    [level 1] Title: 모의고사, Time: 7.29 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,32 +2,27 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[] student1 = {1, 2, 3, 4, 5};
-        int[] student2 = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] student3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        HashMap<Integer, Integer> map = new HashMap<>();
+        int[][] student = {{1, 2, 3, 4, 5}, {2, 1, 2, 3, 2, 4, 2, 5}, {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}};
+        int[] count = new int[3];
         
-        for(int i = 0; i < answers.length; i++) {
-            if(answers[i] == student1[i % 5]) map.put(1, map.getOrDefault(1, 0) + 1);
-            if(answers[i] == student2[i % 8]) map.put(2, map.getOrDefault(2, 0) + 1);
-            if(answers[i] == student3[i % 10]) map.put(3, map.getOrDefault(3, 0) + 1);
-        }
-        
-        int max = 0; 
-        TreeSet<Integer> results = new TreeSet<>();
-        for(int i = 1; i <= 3; i++) {
-            max = Math.max(map.getOrDefault(i, 0), max);
-        }
-        for(int i = 1; i <= 3; i++) {
-            if(max == map.getOrDefault(i,0)) {
-                results.add(i);
+        for(int i = 0; i < student.length; i++ ) {
+            int c = 0;
+            for(int j = 0; j < answers.length; j++) {
+                if(answers[j] == student[i][j % student[i].length]) c++;
             }
+            count[i] = c;
         }
         
-        int[] answer = new int[results.size()];
-        for(int i = 0; i < answer.length; i++) {
-            answer[i] = results.pollFirst();
+        int max = 0;
+        for(int i = 0; i < count.length; i++) {
+            if(count[i] >= max) max = count[i];
         }
-        return answer;
+        
+        TreeSet<Integer> set = new TreeSet<>();
+        for(int i = 0; i < count.length; i++) {
+            if(count[i] == max) set.add(i + 1);
+        }
+        
+        return set.stream().mapToInt(Integer::intValue).toArray();
     }
 }
