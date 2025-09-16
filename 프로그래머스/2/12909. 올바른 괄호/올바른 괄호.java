4b7e6f2001aa9f2605cdf5c53609388d90dabf0e
Gyuseon Sim <118058218+gyuseon25@users.noreply.github.com>
2025-09-16 16:06:05 +0900

    [level 2] Title: 올바른 괄호, Time: 25.58 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,23 +1,17 @@
+import java.util.*;
+
 class Solution {
     boolean solution(String s) {
 
-        int count = 0;
-        for(int i = 0;  i < s.length(); i++) {
-            char tmp = s.charAt(i);
-            if(tmp == '(') {
-                count++;
-            } else {
-                count--;
-            }
-            
-            if(count < 0) {
-                return false;
-            }
+        int lc = 0, rc = 0;
+        Stack<Character> st = new Stack<>();
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(st.isEmpty() && c == ')') return false;
+            else if(c == ')' && st.peek() == '(') st.pop();
+            else st.push(c);
         }
-        
-        if(count != 0) {
-            return false;
-        }
-        return true;
+
+        return st.isEmpty();
     }
 }
