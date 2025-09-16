2334e04147021c477f74a88793ee4efcb6b9bbb9
Gyuseon Sim <118058218+gyuseon25@users.noreply.github.com>
2025-09-16 15:50:30 +0900

    [level 2] Title: 방문 길이, Time: 17.74 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -2,34 +2,36 @@ import java.util.*;
 
 class Solution {
     
-    private boolean isValid(int x, int y) {
-        return x >= -5 && x <= 5 && y >= -5 && y <= 5;
+    private static boolean isValidMove(int nx, int ny) {
+        return nx >= -5 && nx <= 5 && ny >= -5 && ny <= 5;
+    }
+    
+    private static final HashMap<Character, int[]> location = new HashMap<>();
+    
+    private static void initLocation() {
+        location.put('U', new int[] {0, 1});
+        location.put('D', new int[] {0, -1});
+        location.put('L', new int[] {-1, 0});
+        location.put('R', new int[] {1, 0});
     }
     
     public int solution(String dirs) {
+        initLocation();
+        
         int x = 0, y = 0;
         HashSet<String> answer = new HashSet<>();
-        
         for(int i = 0; i < dirs.length(); i++) {
-            int dx = 0, dy = 0;
-            char dir = dirs.charAt(i);
-    
-            if (dir == 'U') dy = 1;
-            else if (dir == 'D') dy = -1;
-            else if (dir == 'R') dx = 1;
-            else if (dir == 'L') dx = -1;
-
-            int nx = x + dx;
-            int ny = y + dy;
-            
-            if (!isValid(nx, ny)) continue;
-
+            int[] offset = location.get(dirs.charAt(i));
+            int nx = x + offset[0];
+            int ny = y + offset[1];
+            if(!isValidMove(nx, ny)) continue;
             answer.add(x + " " + y + " " + nx + " " + ny);
             answer.add(nx + " " + ny + " " + x + " " + y);
+            
             x = nx;
             y = ny;
         }
+        
         return answer.size() / 2;
     }
-    
 }
