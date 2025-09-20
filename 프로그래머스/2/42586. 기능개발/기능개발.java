cac0daf350ce965c8f7215681e015c6853bf9fc9
Gyuseon Sim <118058218+gyuseon25@users.noreply.github.com>
2025-09-20 15:02:26 +0900

    [level 2] Title: 기능개발, Time: 6.56 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,39 +2,23 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        ArrayList<Integer> answer = new ArrayList<>();
-        Queue<Integer> queue = new ArrayDeque<>();
-        
+        Deque<Integer> q = new ArrayDeque<>();
+        List<Integer> list = new ArrayList<>();
         for(int i = 0; i < progresses.length; i++) {
-            int remainder = 100 - progresses[i];
-            if(remainder % speeds[i] == 0) {
-                queue.add(remainder / speeds[i]);
-            } else {
-                queue.add((remainder / speeds[i]) + 1);
-            }
+            int complete = (int) Math.ceil((double) (100 - progresses[i]) / speeds[i]);
+            q.addLast(complete);
         }
         
-        int count = 1;
-        int pre = queue.poll();
-        while(!queue.isEmpty()) {
-            int tmp = queue.poll();
-            if(pre >= tmp) {
+        while(!q.isEmpty()) {
+            int count = 1;
+            int first = q.pollFirst();
+            while(!q.isEmpty() && q.peek() <= first) {
+                q.pollFirst();
                 count++;
-            } else {
-                answer.add(count);
-                count = 1;
-                pre = tmp;
             }
+            list.add(count);
         }
         
-        answer.add(count);
-        
-        
-        int[] convert = new int[answer.size()];
-        for(int i = 0; i < answer.size(); i++) {
-            convert[i] = answer.get(i);
-        }
-        
-        return convert;
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
