9171df76571c495b4bc12486894589651ba7b884
Gyuseon Sim <118058218+gyuseon25@users.noreply.github.com>
2025-09-22 17:31:09 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 84.92 ms, Memory: 98.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,22 +2,17 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        
         HashMap<String, Integer> map = new HashMap<>();
-        
         for(String s : completion) {
             map.put(s, map.getOrDefault(s, 0) + 1);
         }
         
         for(String s : participant) {
-            if(!map.containsKey(s) || map.get(s) == 0){
-                return s;
-            }
-            if(map.containsKey(s)) {
-                map.put(s, map.get(s) - 1); 
-            }
+            if(!map.containsKey(s)) return s;
+            else if(map.get(s) == 1) map.remove(s);
+            else map.put(s, map.get(s) - 1);
         }
         
-        return null;
+        return "";
     }
 }
