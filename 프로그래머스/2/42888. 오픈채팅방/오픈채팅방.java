47b5b2c95bbc0b36f3144dc2fd2c1ce485862616
Gyuseon Sim <118058218+gyuseon25@users.noreply.github.com>
2025-09-26 23:21:28 +0900

    [level 2] Title: 오픈채팅방, Time: 154.93 ms, Memory: 200 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -2,31 +2,50 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] record) {
-        List<String> answer = new ArrayList<>();
+        ArrayList<String> list = new ArrayList<>();
+        HashMap<String, String> nickname = new HashMap<>(); 
+        ArrayList<Status> recordList = new ArrayList<>();
+        HashMap<String, String> convert = new HashMap<>();
         
-        HashMap<String, String> map = new HashMap<>();
+        convert.put("Enter", "님이 들어왔습니다.");
+        convert.put("Leave", "님이 나갔습니다.");
+
         for(String s : record) {
-            String[] str = s.split(" ");
-            if(str[0].equals("Enter")) {
-                map.put(str[1], str[2]);
-            } else if(str[0].equals("Change")) {
-                map.put(str[1], str[2]);
+            String[] sp = s.split(" ");
+            
+            String s1 = sp[0];
+            
+            if(s1.equals("Enter")) {
+                nickname.put(sp[1], sp[2]);
+                Status status = new Status(s1, sp[1]);
+                recordList.add(status);
+            } else if(s1.equals("Leave")) {
+                Status status = new Status(s1, sp[1]);
+                recordList.add(status);
+            } else {
+                nickname.put(sp[1], sp[2]);
             }
         }
         
-        for(String s : record) {
-            String[] str = s.split(" ");
-            if(str[0].equals("Enter")) {
-                answer.add(map.get(str[1]) + "님이 들어왔습니다.");
-            } else if(str[0].equals("Leave")) {
-                answer.add(map.get(str[1]) + "님이 나갔습니다.");
-            }
+        for(Status s : recordList) {
+            String status = convert.get(s.status);
+            String name = nickname.get(s.uid);
+            list.add(name + status);
         }
         
-        String[] result = new String[answer.size()];
-        for(int i = 0 ; i < result.length; i++) {
-            result[i] = answer.get(i);
+        String[] answer = new String[recordList.size()];
+        for(int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
         }
-        return result;
+        return answer;
+    }
+    class Status {
+        String status;
+        String uid;
+        
+        public Status(String status, String uid) {
+            this.status = status;
+            this.uid = uid;
+        }
     }
 }
