f3ec8900b6845be218c3c9732e0433f25e54ccc8
Gyuseon Sim <118058218+gyuseon25@users.noreply.github.com>
2025-09-27 13:52:11 +0900

    [level 1] Title: 신고 결과 받기, Time: 165.13 ms, Memory: 175 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -2,45 +2,38 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
+        HashMap<String, HashSet<String>> record = new HashMap<>();
+        HashMap<String, Integer> counts = new HashMap<>();
         
-        HashMap<String, List<String>> record = new HashMap<>();
-        HashMap<String, Integer> count = new HashMap<>();
-        
-        for(String str : report) {
-            String[] split = str.split(" ");
-            
-            String 신고인 = split[0];
-            String 피신고인 = split[1];
-            
-            if(record.get(신고인) == null) {
-                record.put(신고인, new ArrayList<String>());
+        for(int i = 0; i < report.length; i++) {
+            String[] sp = report[i].split(" ");
+            String from = sp[0];
+            String to = sp[1];
+            record.putIfAbsent(from, new HashSet<>());
+            HashSet<String> set = record.get(from);
+            if(set.contains(to)) {
+                continue;
+            } else {
+                set.add(to);
+                counts.put(to, counts.getOrDefault(to, 0) + 1);
             }
-            
-            if(record.get(신고인).contains(피신고인)){
-                 continue;   
-            }            
-            
-            count.put(피신고인, count.getOrDefault(피신고인, 0) + 1);
-            record.get(신고인).add(피신고인);
         }
         
-        int[] answer = new int[id_list.length];
-        
-        for(int i = 0; i < answer.length; i++) { 
-            int tmp = 0;
+        int[] answer = new int[id_list.length];    
+
+        for(int i = 0; i < id_list.length; i++) {
             String id = id_list[i];
+            int count = 0;
             
-            List<String> ls = record.getOrDefault(id, null);
-            if(ls != null) {
-             for(String s : ls) {
-                if(count.getOrDefault(s, 0) >= k){
-                    tmp++;
-                }
-            }   
+            HashSet<String> set = record.get(id);
+            if(set == null) continue;
+            for(String s : set) {
+                if(counts.getOrDefault(s, 0) >= k) count++;
             }
-            answer[i] = tmp;
+            answer[i] = count;
         }
         
+    
         return answer;
     }
 }
