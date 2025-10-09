4cfb5f034b487d9405f27ce8abd3975805c85341
Gyuseon Sim <118058218+gyuseon25@users.noreply.github.com>
2025-10-09 21:43:37 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.08 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -2,29 +2,24 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n, String[] words) {
-        int[] answer = new int[]{0, 0};
-
+        int[] answer = {0, 0};
         HashSet<String> set = new HashSet<>();
-        
-        int count = 1;
-        char last = words[0].charAt(0);
-        
-        for(String word : words) {
-            if(word.charAt(0) != last || set.contains(word)) {
-                if(count % n == 0) {
-                    answer[0] = n;
-                } else {
-                    answer[0] = count % n;
-                }
-                answer[1] = (int) Math.ceil((double) count / n);
-                break;
+
+        char last = words[0].charAt(words[0].length() - 1);
+        set.add(words[0]);
+
+        for (int i = 1; i < words.length; i++) {
+            String word = words[i];
+
+            if (set.contains(word) || word.charAt(0) != last) {
+                answer[0] = (i % n) + 1;
+                answer[1] = (i / n) + 1;
+                return answer;
             }
-            
+
             set.add(word);
-            count++;
             last = word.charAt(word.length() - 1);
         }
-
         return answer;
     }
 }
